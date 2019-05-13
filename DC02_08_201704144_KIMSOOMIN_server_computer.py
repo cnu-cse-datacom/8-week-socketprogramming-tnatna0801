89b7aaf157691ae9544ee7b199b20b466dda59eb
tnatna0801 <48270067+tnatna0801@users.noreply.github.com>
2019-05-14 00:56:41 +0900

    edit

---
+++ DC02_08_201704144_KIMSOOMIN_server_computer.py
@@ -0,0 +1,46 @@
+import socket
+import os
+FLAGS = None
+
+#
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--ip', type=str, default = 'localhost')
+parser.add_argument('-p', '--post', type=int, default = '9100')
+FLAGS, _ = parser.parse_known_args()
+
+#socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((FLAGS.ip, FLAGS.post))
+
+#receive
+filename, addr = server_socket.recvfrom(1024)
+size, addr = server_socket.recvfrom(1024)
+file_size = int(size.decode())
+#file_size = size.decode()
+
+#print
+print("file recv start from", addr[0])
+
+print("FILE NAME : ", filename.decode()) #decode()
+print("FILE SIZE : ", file_size) #decode()
+
+#file open
+f = open(filename.decode(), 'wb')
+
+current_size = 0
+
+while current_size < file_size:
+
+    data, addr = server_socket.recvfrom(1024)
+    current_size = min(current_size + 1024, file_size)
+    f.write(data)
+
+    print("current_size / total_size = ", current_size, "/", file_size, ", ", (current_size/file_size) * 100, "%",)
+    #print("\n")
+
+f.close()
+print("yeah")
+server_socket.sendto("ok".encode(), addr)
+server_socket.sendto("file_send_end".encode(), addr)
+server_socket.close()
